e68740a41539ace761280e9f39cded5afa4f0243
Jacky Alciné <me@jalcine.me>
2015-02-28 18:55:09 -0500

    maggggicccc.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,14 +1,22 @@
+'use strict';
+
 $(function blkMgc() {
-    var blackPeopleData = {
-    };
     var blackPeopleData = [
-        {'name':'George Edward Alcorn, Jr.'},
-        {'name':'Patricia Bath'},
-        {'name':'Andrew Jackson Beard'},
-        {'name':'Miriam Benjamin'},
-        {'name':'Henry Blair (inventor)'},
-        {'name':'Bessie Blount Griffin'},
-        {'name':'Sarah Boone'},
-        {'name':'Otis Boykin'}
-    ]
+        {'name':'George Edward Alcorn, Jr.', 'img': 'http://www.cpnas.org/aahp/biographies/primary-biography-photos/alcorn_george.jpg'},
+        {'name':'Patricia Bath', 'img': ''},
+        {'name':'Andrew Jackson Beard', 'img': ''},
+        {'name':'Miriam Benjamin', 'img': ''},
+        {'name':'Henry Blair (inventor)', 'img': ''},
+        {'name':'Bessie Blount Griffin', 'img': ''},
+        {'name':'Sarah Boone', 'img': 'http://www.slidepoint.net/users/hhtp/_images/hhuu/sarah_boone%5B2%5D.jpg'},
+        {'name':'Otis Boykin', 'img': 'https://upload.wikimedia.org/wikipedia/commons/4/43/Boykins_at_Dept_of_Energy_BLACK_CONTRIBUTORS_TO_SCIENCE_AND_ENERGY_TECHNOLOGY_Page_08.jpg'}
+    ];
+
+    var fields = $('.box');
+    fields.each(function(i, elem){
+        var name = blackPeopleData[i].name;
+        var img = blackPeopleData[i].img;
+        $(elem).css('background-image', "url('"+img+"')");
+        $(elem).attr('title', name);
+    });
 });
